9f852792c5541c223d1e9ac4a7ce8589b3825bcb
Martin Bago <132388058+martinbago22@users.noreply.github.com>
2023-09-26 11:58:08 +0200

    create Event component, set up static middleware and a new proxy to serve our image file

---
+++ client/src/components/Event.jsx
@@ -0,0 +1,31 @@
+export default function Event({ event }) {
+  return (
+    <div className="Event">
+      <div className="EventName">{event.name}</div>
+      <div className="EventIMG">
+        <img src={event.image}></img>
+      </div>
+      <div className="EventInfo">
+        <ul>
+          <li>
+            <label>Date: </label>
+            {event.date}
+          </li>
+          <li>
+            <label>Location: </label>
+            {event.location}
+          </li>
+          <li>
+            <label>Price: </label>
+            {event.price} €
+          </li>
+        </ul>
+      </div>
+      <div className="EventDetails">{event.details}</div>
+      <div className="EventButtons">
+        <button>DEL</button>
+        <button>EDIT</button>
+      </div>
+    </div>
+  )
+}
--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import * as path from 'path';
 
 dotenv.config();
 const MONGO_URL = process.env.MONGO_URL;
 
 const app = express();
 app.use(express.json());
+app.use('/public/', express.static(path.resolve('./public')));
+
 
 
 async function main() {
